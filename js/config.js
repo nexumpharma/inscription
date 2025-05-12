a474c2e0bc961714aeb9ffe724e2bc302011126f
nexumpharma <samuel@nexum-technologies.fr>
2025-05-12 08:32:05 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// js/config.js
+export const SUPABASE_URL = 'https://nywocoxkfdrsscvnvjas.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55d29jb3hrZmRyc3Njdm52amFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4OTA1MDgsImV4cCI6MjA2MjQ2NjUwOH0.Gj2TOFLghSlztKgZiUzrdnntH5oNPKs8RALG8fGvSm4';
