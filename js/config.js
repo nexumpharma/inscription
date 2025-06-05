62ef38d4424ef906da5ff758240b938de411638f
nexumpharma <samuel@nexum-technologies.fr>
2025-06-05 13:15:38 +0200

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-// js/config.js
-export const SUPABASE_URL = 'https://nywocoxkfdrsscvnvjas.supabase.co';
-export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im55d29jb3hrZmRyc3Njdm52amFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4OTA1MDgsImV4cCI6MjA2MjQ2NjUwOH0.Gj2TOFLghSlztKgZiUzrdnntH5oNPKs8RALG8fGvSm4';
-export const SUPABASE_FUNCTION_BASE = 'https://nywocoxkfdrsscvnvjas.functions.supabase.co';
